008f681453be4c2aef028afdf3860544a4a690bd
Maciel Gonzales <leicam10@outlook.com>
2021-11-20 17:35:18 -0500

    2nd commit

---
+++ routes/PagoRouter.js
@@ -0,0 +1,8 @@
+// const router = require('express').Router()
+// const pagoController = require('../controllers/PagoController')
+// const auth = require('../middleware/auth')
+// const authAdmin = require('../middleware/authAdmin')
+
+// router.route('/pago')
+//       .get(auth, authAdmin, pagoController.getPago)
+//       .post(auth, pagoController.crearPago)
